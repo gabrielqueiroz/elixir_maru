2458e745255ecb5c63189f33a06045ee5459649b
gabriel.queiroz <gabriel.fatec@hotmail.com>
2018-03-19 17:01:01 -0300

    Create maru project with mix

---
+++ lib/elixir_maru.ex
@@ -0,0 +1,18 @@
+defmodule ElixirMaru do
+  @moduledoc """
+  Documentation for ElixirMaru.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirMaru.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
